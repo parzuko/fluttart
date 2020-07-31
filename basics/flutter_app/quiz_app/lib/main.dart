4b1ea33246061a52a59bc9dbf89ca8fb532c2c0f
parzuko <jivansh17@gmail.com>
2020-07-31 17:25:52 +0530

    Added Scaffold class and Appbar to it

--- basics/flutter_app/quiz_app/lib/main.dart
+++ basics/flutter_app/quiz_app/lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
+  //build method which returns a Widget
   Widget build(BuildContext context) {
+    // Widget is the class MaterialApp
     return MaterialApp(
-      home: Text("hello, friend"),
+      home: Scaffold(
+        //Class built to make a base screen
+        appBar: AppBar(
+          title: Text("My First App"),
+        ), // Scaffold class takes a positonal argument as an app bar whcih takes a title
+        body: Text("Lorem Ipsum Stuff"),
+      ),
     );
   }
 }
