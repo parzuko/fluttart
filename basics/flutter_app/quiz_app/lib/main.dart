055ce3cf442471c25fa6e6efdd0217d70be52660
parzuko <jivansh17@gmail.com>
2020-07-31 17:38:53 +0530

    Invisible Widgets used for Stylinng (Columns)

--- basics/flutter_app/quiz_app/lib/main.dart
+++ basics/flutter_app/quiz_app/lib/main.dart
@@ -11,13 +11,33 @@ class MyApp extends StatelessWidget {
   //build method which returns a Widget
   Widget build(BuildContext context) {
     // Widget is the class MaterialApp
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?"
+    ];
     return MaterialApp(
       home: Scaffold(
         //Class built to make a base screen
         appBar: AppBar(
           title: Text("My First App"),
         ), // Scaffold class takes a positonal argument as an app bar whcih takes a title
-        body: Text("Lorem Ipsum Stuff"),
+        body: Column(
+          children: [
+            Text("The questions is!: "),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
